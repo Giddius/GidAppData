38b78391666d0efa1a75211cd58e29a8e85b6470
Giddius <GordonGrossmann@gmx.at>
2021-02-12 02:35:58 +0100

    a

--- gidappdata/__init__.py
+++ gidappdata/__init__.py
@@ -2,7 +2,7 @@
 GidAppData provides an Userdata deployment, with appdirs as backendend, but easy access to files saved there. Also provides an option to 'fake' deploy it while still developing
 """
 
-__version__ = '0.1.12'
+__version__ = '0.1.13'
 
 
 from gidappdata.standard_appdata import *
